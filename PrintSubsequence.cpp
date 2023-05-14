6442a8d39ea3e0173462a91bf0efd96385e80fcf
Akash_Ktr <akash.katiyar.met21@itbhu.ac.in>
2023-05-14 19:10:06 +0530

    Subsequence 1

--- PrintSubsequence.cpp
+++ PrintSubsequence.cpp
@@ -1,4 +1,4 @@
-// A subsequence is a contiguous/ non-contigous sequence which follows an order
+// A subsequence is a contiguous/ non-contigous sequence which follows the order
 #include<bits/stdc++.h>
 #include<iostream>
 #include<algorithm>
@@ -18,8 +18,42 @@
 #define ll long long int
 using namespace std;
 
+void sub(int i, int arr[], vector<int> seq, int n){
+   if(i>=n){
+      // printing the subsequence when base case is met
+      for (int i = 0; i < seq.size(); i++)
+      {
+         cout<<seq[i]<<" ";
+      }
+      cout<<"\n";
+      return;
+   }
+
+   // two options: 1. to take an element 2.to not take an element
+
+   // taking i'th element
+   seq.push_back(arr[i]);
+   sub(i+1, arr, seq, n);
+
+   // not taking i'th element
+   seq.pop_back();
+   sub(i+1, arr, seq, n);
+}
+
 int main()
 {
+   int n; cin>>n;
+   int arr[n];
+   for (int i = 0; i < n; i++)
+   {
+      cin>>arr[i];
+   }
+
+   vector<int> seq;
+   sub(0,arr, seq, n);
    
    return 0;
 }
+
+// TC O(n*2^n)
+// SC O(n)
